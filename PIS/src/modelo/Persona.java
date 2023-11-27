6a3c44d08f5839d2d9ef2d76476e3dde8ecf1c37
Brian Fernando Aguinsaca L <brian.aguinsaca@unl.edu.ec>
2023-11-26 21:53:30 -0500

    Creacion Clase Persona

---
+++ PIS/src/modelo/Persona.java
@@ -0,0 +1,76 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author Usuario iTC
+ */
+public class Persona {
+    
+    //Atributos
+    private Integer id;
+    private String nombres;
+    private String apellidos;
+    private Integer edad;
+    private String cedula;
+    
+    
+    //Constructor
+
+    public Persona(Integer id, String nombres, String apellidos, Integer edad, String cedula) {
+        this.id = id;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.edad = edad;
+        this.cedula = cedula;
+    }
+    
+    
+    
+    //Getter and Setter
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+    
+    
+}
